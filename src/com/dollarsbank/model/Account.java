9d281b04f7bf54773b91d5ea11dfc53cfbeb7761
tszkla2 <tom_sz@comcast.net>
2021-09-24 10:58:19 -0500

    Started savings account model

--- src/com/dollarsbank/model/Account.java
+++ src/com/dollarsbank/model/Account.java
@@ -2,19 +2,12 @@ package com.dollarsbank.model;
 
 import java.util.List;
 
-public class Account {
+public abstract class Account {
 	
-	private double money;
-	private String accountID;
-	private String transaction;
-	
-	
-	
-	
-//	public abstract void withdraw(double money);
-//	public abstract void deposit(double money);
-//	public abstract List<String> get5History();
-//	public abstract void addHistory(String transaction);
-//	public abstract void displayHiistory();
-//	public abstract void transfer(double money, String accountID);
+	public abstract void withdraw(double money);
+	public abstract void deposit(double money);
+	public abstract List<String> get5History();
+	public abstract void addHistory(String transaction);
+	public abstract void displayHiistory();
+	public abstract void transfer(double money, String accountID);
 }
--- src/com/dollarsbank/model/SavingsAccount.java
+++ src/com/dollarsbank/model/SavingsAccount.java
@@ -1,5 +1,44 @@
 package com.dollarsbank.model;
 
+import java.util.List;
+
 public class SavingsAccount extends Account {
+
+	@Override
+	public void withdraw(double money) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deposit(double money) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<String> get5History() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addHistory(String transaction) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayHiistory() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void transfer(double money, String accountID) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
