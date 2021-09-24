4030ccf272f5877140de1617fa6730b2a2e65e06
tszkla2 <tom_sz@comcast.net>
2021-09-24 10:27:59 -0500

    Created Abstract Account model

--- src/com/dollarsbank/application/DollarsBankApplication.java
+++ src/com/dollarsbank/application/DollarsBankApplication.java
@@ -1,5 +1,20 @@
 package com.dollarsbank.application;
 
+import com.dollarsbank.controller.DollarsBankController;
+
 public class DollarsBankApplication {
 
+	public static void main(String[] args)
+	{
+		
+		DollarsBankController dollarsBankController = new DollarsBankController();
+		Boolean run = true;
+		
+		while(run)
+		{
+			run = dollarsBankController.run();
+			
+		}
+			
+	}
 }
--- src/com/dollarsbank/model/Account.java
+++ src/com/dollarsbank/model/Account.java
@@ -1,5 +1,12 @@
 package com.dollarsbank.model;
 
-public class Account {
+import java.util.List;
 
+public abstract class Account {
+	
+	public abstract void withdraw(double money);
+	public abstract void deposit(double money);
+	public abstract List<String> get5History();
+	public abstract void addHistory(String transaction);
+	public abstract void displayHiistory();
 }
